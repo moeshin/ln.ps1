ae1beac2a261b72672a378a46be0fa0b47c7e8c0
moeshin <764686875@qq.com>
2020-12-21 09:40:14 +0800

    save

--- test.ps1
+++ test.ps1
@@ -7,34 +7,46 @@ ln -s -v -r a /root/test
 '/root/test/a' -> '../../www/a'
 #>
 
-$dir = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($args[1].Replace('/', '\'))
-$tagert = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($args[0].Replace('/', '\'))
-
-$dir
-$tagert
-
-
-$a1 = $dir.split('\')
-$a2 = $tagert.split('\')
-
-$len1 = $a1.Count
-$len2 = $a2.Count
-if ($len1 -lt $len2) {
-    $len = $len1
-} else {
-    $len = $len2
+function getAbsolutePath($path) {
+    return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path.Replace('/', '\'))
 }
 
-$path = ''
-for ($i = 0; $i -lt $len; ++$i) {
-    if ($a1[$i] -ne $a2[$i]) {
-        $a = $a1[$i..$len1]
-        $a -join '\'
-        $a = $a2[$i..$len2]
-        $a -join '\'
-        break
+function splitPath($path) {
+    return $path.split('\', [StringSplitOptions]::RemoveEmptyEntries)
+}
+
+$dir = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($args[0].Replace('/', '\'))
+$tagert = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($args[1].Replace('/', '\'))
+
+function getRelativePath($dir, $target) {
+    $dir = getAbsolutePath($dir)
+    $target = getAbsolutePath($target)
+    $a1 = splitPath($dir)
+    $a2 = splitPath($target)
+    
+    $len1 = $a1.Count
+    $len2 = $a2.Count
+    $len = if ($len1 -lt $len2) {$len1} else {$len2}
+
+    $path = ''
+    for ($i = 0; $i -lt $len; ++$i) {
+        if ($a1[$i] -ne $a2[$i]) {
+            if ($i -eq 0) {
+#                "Cannot find relative path on different disk"
+#                return $target
+            }
+        $n = $len1 - $i - 1
+        if ($n -ge 0)  {
+            $path += '..\' * $n
+        }
+        $path += $a2[$i..$len2] -join '\'
+        
+        return $path
     }
 }
 
+"PATH: $path"
 
-#Cannot find relative path on different disk
+}
+
+
